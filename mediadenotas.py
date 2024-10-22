06a315b1b27b92b2c2ea778423c837e85b08c4a4
angelyromu <granadosrodriguezangel246@gmail.com>
2024-10-22 11:02:42 +0200

    Add files via upload

---
+++ mediadenotas.py
@@ -0,0 +1,16 @@
+#calculo de la media de nota
+'''
+
+Escribe un programa que pida al usuario tres
+ notas de exámenes y calcule la media aritmética
+
+
+'''
+print("dime la nota1")
+nota1 = float(input())
+print("dime la nota 2")
+nota2 = float(input())
+print("DIME LA NOTA 3")
+nota3 = float(input())
+media = (nota1+nota2+nota3)/3
+print("el resultado de la nota es",media)
